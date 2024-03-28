8b316f2074c338d004612020caf8e2d9e60f2a3a
Shaolong Li <shaolongli26@gmail.com>
2024-03-27 17:13:26 -0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+//cannot directly use process.env in backend, npm i dotenv first
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-
 export default function About() {
   return (
     <div>
-      <h1>
-        About
-      </h1>
+      <h1>About</h1>
     </div>
-  )
+  );
 }
