eec31325881430f0f744319f22b2e62842c31842
Shaolong Li <shaolongli26@gmail.com>
2024-03-30 23:00:56 -0700

    add google sign in api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin, google } from "../controllers/auth.controller.js";
 
 //user router to make CRUD operation, what to do exactly is specified in controller
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post("/google");
+router.post("/google", google);
 
 export default router;
