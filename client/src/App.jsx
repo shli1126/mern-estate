2f6f71ebfd9590de81e299c6f7a81d8bee27bc58
Shaolong Li <shaolongli26@gmail.com>
2024-06-29 21:15:08 -0700

    add swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import About from "./pages/About";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (
@@ -16,7 +18,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
